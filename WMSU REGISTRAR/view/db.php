2729f75f206be58daa0972516b5148c5b40bed94
JanbresGagaracruz <134786662+COPYARUL@users.noreply.github.com>
2023-05-28 00:40:44 +0800

    G

--- WMSU REGISTRAR/view/db.php
+++ WMSU REGISTRAR/view/db.php
@@ -1,16 +1,30 @@
 <?php
-$servername = "localhost"; // Change this to your server name if applicable
-$username = "DESKTOP-89QNA16/amazi"; // Change this to your database username
-$password = ""; // Change this to your database password
-$dbname = "THESIS"; // Change this to your database name
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+function insertUser( $userName, $userEmail, $userCourse) {
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "WMSU"; 
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    echo "Connected successfullyd";
+    
+    $query = "INSERT INTO `user`(`USER_NAME`, `USER_EMAIL`, `USER_COURSE`)
+              VALUES ( '$userName', '$userEmail', '$userCourse')";
+    if ($conn->query($query) === TRUE) {
+        echo "User inserted successfully.";
+    } else {
+        echo "Error: " . $query . "<br>" . $conn->error;
+    }
+    $conn->close();
 }
 
-echo "Connected successfully";
+
+insertUser('Jb', 'jb@example.com', 'Computer Science');
+
 ?>
